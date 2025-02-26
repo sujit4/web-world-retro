a4d9ff3c4200edbc81b84c6523b1ce98cb51fe4f
VyvorThePaladin <4795034-VyvorThePaladin@users.noreply.replit.com>
2025-02-26 23:35:35 +0000

    Remove page transition animation and simplify routing.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/99b88217-3bcb-453c-8848-6b1bf4910d15/512394b6-aa5b-4699-b320-6710b22ab516.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,18 @@
-import { Switch, Route, useLocation } from "wouter";
+import { Switch, Route } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { AnimatePresence } from "framer-motion";
 import { queryClient } from "./lib/queryClient";
-import PageTransition from "@/components/PageTransition";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Blog from "@/pages/Blog";
 
 function Router() {
-  const [location] = useLocation();
-
   return (
-    <AnimatePresence mode="wait">
-      <Switch key={location}>
-        <Route path="/">
-          <PageTransition>
-            <Home />
-          </PageTransition>
-        </Route>
-        <Route path="/blog">
-          <PageTransition>
-            <Blog />
-          </PageTransition>
-        </Route>
-        <Route>
-          <PageTransition>
-            <NotFound />
-          </PageTransition>
-        </Route>
-      </Switch>
-    </AnimatePresence>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/blog" component={Blog} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
