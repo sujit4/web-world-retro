9c3d689d67260baa6210c42411d6c9ac1f19a8f3
Sujit Konapur <skonapur@terpmail.umd.edu>
2025-03-01 13:40:48 -0800

    Update Vercel configuration to properly serve the application

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
+// This file will be built by esbuild during the build process
+// The actual code will be generated from server/index.ts
+
 // This file is used as a proxy to the server/index.js file
 // It's needed because Vercel expects serverless functions to be in the api directory
 export * from '../dist/index.js'; 
