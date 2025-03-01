47017e9ef2d4306f14fd37de63fe193eef23f0a9
Sujit Konapur <skonapur@terpmail.umd.edu>
2025-03-01 13:39:42 -0800

    Fix Vercel deployment to properly serve Express.js backend

---
+++ api/index.js
@@ -0,0 +1,3 @@
+// This file is used as a proxy to the server/index.js file
+// It's needed because Vercel expects serverless functions to be in the api directory
+export * from '../dist/index.js'; 
